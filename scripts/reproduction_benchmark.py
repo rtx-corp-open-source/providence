bcaf7579675fdb16668ea5b3f99c95a2f5e04607
Stephen Fox <stephen.fox@rtx.com>
2023-05-19 15:28:18 -0400

    Format and document the reproduction_benchmark

--- scripts/reproduction_benchmark.py
+++ scripts/reproduction_benchmark.py
@@ -1,10 +1,12 @@
 """
 This script captures the key-value correspondence between a reference model and their best case performance
-given all of the parameters we have available to configure the space.
-If you follow this file, you should get *better* results than were published in Providence
-(work avaiable to read at https://ieeexplore.ieee.org/document/9843469 or by DOI: 10.1109/AERO53065.2022.9843469)
+given all of the parameters we have available in configuration space (and budget to test).
 
-Include everything for the best performance on the NASA FD00X subsets
+If you follow this file, you should get *better* results than were published in Providence, as noted by the ``best_metrics``
+bound to each instantiation.
+(Providence is avaiable to read at https://ieeexplore.ieee.org/document/9843469 or by DOI: 10.1109/AERO53065.2022.9843469)
+
+Includes everything for the best performance on the NASA, Backblaze, and BackblazeExtended datasets for the RNN-based models
 -   Hyperparameters
 -   Seeds
 -   Every line of initialization
@@ -59,9 +61,20 @@ set_torch_default_dtypes(pt.float32)
 
 
 def callback_default_config(num_epochs: int = 500) -> dict:
+    """Default configuration for Callbacks used in training. See ``providence_utils.callbacks.py`` for more
+
+    Args:
+        num_epochs: Number of epochs used it training; used to derive upper bounds for metadata / evaluation output frequency. Defaults to 500.
+
+    Returns:
+        configuration settings
+    """
     callback_config = dict()
     callback_config["epoch_logger_epochs"] = 10
     callback_config["visualizer"] = IntervalMetricsVisualizer.__name__
+
+    # with our larger learning rate training, you generally don't need this many epochs
+    # but this is the configuration we experimented with.
     callback_config["visualization_freq"] = min(200, num_epochs // 5)
     callback_config["model_output_freq"] = min(100, num_epochs // 2)
     callback_config["n_kept_checkpoints"] = 5
@@ -74,9 +87,14 @@ def callback_default_config(num_epochs: int = 500) -> dict:
 
 
 def optimizer_default_config() -> dict:
+    """Returns default configuration for the optimizers used in these benchmarks.
+    """
     optim_config = dict()
     optim_config["schedule_min"] = 1e-5
     optim_config["schedule_T_mult"] = 2
+    # Adam works well. SGD needs more effort in the LR Schedule. The schedule can be done without, but is more useful when
+    # using SGD optimizers. Omitted here, because it didn't beat out Adam in our experimentation
+    optim_config["type"] = pt.optim.Adam
     return optim_config
 
 
@@ -167,49 +185,28 @@ feature_counts = {
 # NOTE one of these is redundant, as its covered in the paper_reproductions module
 # NOTE: these statefully set the global seed.
 def NASA_Aggregate_VanillRNN(data_root_path: str, outputs_root: str):
-    """
+    """Train a vanilla RNN on the NASA Aggregate Dataset.
+
     Arguments:
-        data_root_path: the root for the download of the dataset. Needs write permissions
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
         outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
 
     Return:
         model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
-            with the file model checkpoint output by `ModelCheckpointer`
-        metrics: produced by `GeneralMetrics`
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
     """
-    train_ds, val_ds = NasaDatasets(data_root=data_root_path)
-    test_ds = val_ds
-
-    def fresh_dls(bs: int):  # replicate what was done above. Just do it again
-        return CustomProvidenceDataloaders(
-            train_ds,
-            val_ds,
-            batch_size=bs,
-            num_workers=1,
-            pin_memory=True,
-        )._replace(test=ProvidenceDataLoader(test_ds, batch_size=1, num_workers=1))
-
-    optim_params = {
-        **optimizer_default_config(),
-        "batch_size": 4,
-        "learning_rate": 0.003,
-        "num_epochs": 200,
-        "schedule_T_mult": 2,
-        "schedule_min": 1e-05,
-        "type": pt.optim.Adam,
-    }
+    optim_params = dict(**optimizer_default_config(), batch_size=4, learning_rate=3e-3, num_epochs=200)
+    model_params = dict(input_size=feature_counts["nasa"], hidden_size=64, num_layers=2, dropout=0.3)
+    seed = 11068621650300516211
 
     model, metrics = do_model_run(
         ProvidenceVanillaRNN,
-        fresh_dls,
-        random_seed=11068621650300516211,
+        _nasa_dls_func(data_root_path),
+        random_seed=seed,
         optim_params=optim_params,
-        model_params=dict(
-            input_size=feature_counts["nasa"],
-            hidden_size=64,
-            num_layers=2,
-            dropout=0.3,
-        ),
+        model_params=model_params,
         callback_config=callback_default_config(),
         outputs_root_dir=outputs_root,
         epoch_definition=training_epoch,
@@ -228,49 +225,28 @@ NASA_Aggregate_VanillRNN.best_metrics = pd.DataFrame(
 
 
 def NASA_Aggregate_GRU(data_root_path: str, outputs_root: str):
-    """
+    """Train a GRU RNN on the NASA Aggregate Dataset.
+
     Arguments:
-        data_root_path: the root for the download of the dataset. Needs write permissions
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
         outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
 
     Return:
         model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
-            with the file model checkpoint output by `ModelCheckpointer`
-        metrics: produced by `GeneralMetrics`
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
     """
-    train_ds, val_ds = NasaDatasets(data_root=data_root_path)
-    test_ds = val_ds
-
-    def fresh_dls(bs: int):  # replicate what was done above. Just do it again
-        return CustomProvidenceDataloaders(
-            train_ds,
-            val_ds,
-            batch_size=bs,
-            num_workers=1,
-            pin_memory=True,
-        )._replace(test=ProvidenceDataLoader(test_ds, batch_size=1, num_workers=1))
-
-    optim_params = {
-        **optimizer_default_config(),
-        "batch_size": 2,
-        "learning_rate": 1e-3,
-        "num_epochs": 50,
-        "schedule_T_mult": 2,
-        "schedule_min": 1e-05,
-        "type": pt.optim.Adam,
-    }
+    optim_params = dict(**optimizer_default_config(), batch_size=2, learning_rate=1e-3, num_epochs=50)
+    model_params = dict(input_size=feature_counts["nasa"], hidden_size=512, num_layers=3, dropout=0.75)
+    seed = 11068621650300516211
 
     model, metrics = do_model_run(
         ProvidenceGRU,
-        fresh_dls,
-        random_seed=11068621650300516211,
+        _nasa_dls_func(data_root_path),
+        random_seed=seed,
         optim_params=optim_params,
-        model_params=dict(
-            input_size=feature_counts["nasa"],
-            hidden_size=512,
-            num_layers=3,
-            dropout=0.75,
-        ),
+        model_params=model_params,
         callback_config=callback_default_config(),
         outputs_root_dir=outputs_root,
         epoch_definition=training_epoch,
@@ -289,16 +265,41 @@ NASA_Aggregate_GRU.best_metrics = pd.DataFrame(
 
 
 def NASA_Aggregate_LSTM(data_root_path: str, outputs_root: str):
-    """
+    """Train a LSTM RNN on the NASA Aggregate Dataset.
+
     Arguments:
-        data_root_path: the root for the download of the dataset. Needs write permissions
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
         outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
 
     Return:
         model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
-            with the file model checkpoint output by `ModelCheckpointer`
-        metrics: produced by `GeneralMetrics`
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
     """
+    optim_params = dict(**optimizer_default_config(), batch_size=4, learning_rate=3e-3, num_epochs=40)
+
+    model_params = dict(
+        input_size=feature_counts["nasa"],
+        hidden_size=1024,
+        num_layers=3,
+        dropout=0.25,
+    )
+
+    model, metrics = do_model_run(
+        ProvidenceLSTM,
+        _nasa_dls_func(data_root_path),
+        random_seed=11068621650300516211,
+        optim_params=optim_params,
+        model_params=model_params,
+        callback_config=callback_default_config(),
+        outputs_root_dir=outputs_root,
+        epoch_definition=training_epoch,
+    )
+    return model, metrics
+
+
+def _nasa_dls_func(data_root_path):
     train_ds, val_ds = NasaDatasets(data_root=data_root_path)
     test_ds = val_ds
 
@@ -311,32 +312,7 @@ def NASA_Aggregate_LSTM(data_root_path: str, outputs_root: str):
             pin_memory=True,
         )._replace(test=ProvidenceDataLoader(test_ds, batch_size=1, num_workers=1))
 
-    optim_params = {
-        **optimizer_default_config(),
-        "batch_size": 4,
-        "learning_rate": 3e-3,
-        "num_epochs": 40,
-        "schedule_T_mult": 2,
-        "schedule_min": 1e-05,
-        "type": pt.optim.Adam,
-    }
-
-    model, metrics = do_model_run(
-        ProvidenceLSTM,
-        fresh_dls,
-        random_seed=11068621650300516211,
-        optim_params=optim_params,
-        model_params=dict(
-            input_size=feature_counts["nasa"],
-            hidden_size=1024,
-            num_layers=3,
-            dropout=0.25,
-        ),
-        callback_config=callback_default_config(),
-        outputs_root_dir=outputs_root,
-        epoch_definition=training_epoch,
-    )
-    return model, metrics
+    return fresh_dls
 
 
 NASA_Aggregate_LSTM.best_metrics = pd.DataFrame(
@@ -365,15 +341,19 @@ def _backblaze_dls_func(data_root_path: str):
 
 
 def Backblaze_VanillaRNN(data_root_path: str, outputs_root: str):
-    opt_params = dict(
-        **optimizer_default_config(),
-        batch_size=1,
-        learning_rate=3e-3,
-        num_epochs=700,
-        schedule_T_mult=2,
-        schedule_min=1e-5,
-        type=pt.optim.Adam
-    )
+    """Train a vanilla RNN on the Backblaze Dataset.
+
+    Arguments:
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
+        outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
+
+    Return:
+        model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
+    """
+    opt_params = dict(**optimizer_default_config(), batch_size=1, learning_rate=3e-3, num_epochs=700)
     model_params = dict(input_size=feature_counts["backblaze"], hidden_size=128, num_layers=2, dropout=0.6)
     seed = 15620825294243023828
 
@@ -399,16 +379,19 @@ Backblaze_VanillaRNN.best_metrics = pd.DataFrame({
 
 
 def Backblaze_LSTM(data_root_path: str, outputs_root: str):
+    """Train an LSTM RNN on the Backblaze Dataset.
 
-    opt_params = dict(
-        **optimizer_default_config(),
-        batch_size=8,
-        learning_rate=3e-3,
-        num_epochs=700,
-        schedule_T_mult=2,
-        schedule_min=1e-5,
-        type=pt.optim.Adam
-    )
+    Arguments:
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
+        outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
+
+    Return:
+        model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
+    """
+    opt_params = dict(**optimizer_default_config(), batch_size=8, learning_rate=3e-3, num_epochs=700)
     model_params = dict(input_size=feature_counts["backblaze"], hidden_size=512, dropout=0.75, num_layers=3)
     seed = 11068621650300516211
 
@@ -425,9 +408,19 @@ Backblaze_LSTM.best_metrics = pd.DataFrame({"MSE": [788.1], "MFE": [1.68], "SMAP
 
 
 def Backblaze_GRU(data_root_path: str, outputs_root: str):
-    opt_params = dict(
-        batch_size=64, learning_rate=3e-3, num_epochs=700, schedule_T_mult=2, schedule_min=1e-5, type=pt.optim.Adam
-    )
+    """Train a GRU RNN on the Backblaze Dataset.
+
+    Arguments:
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
+        outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
+
+    Return:
+        model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
+    """
+    opt_params = dict(**optimizer_default_config(), batch_size=64, learning_rate=3e-3, num_epochs=700)
     model_params = dict(input_size=feature_counts["backblaze"], hidden_size=1024, dropout=0.3, num_layers=2)
     seed = 11068621650300516211
 
@@ -449,8 +442,20 @@ Backblaze_GRU.best_metrics = pd.DataFrame({
 
 
 def BackblazeExtended_VanillaRNN(data_root_path: str, outputs_root: str):
+    """Train a vanilla RNN on the BackblazeExtended Dataset.
+
+    Arguments:
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
+        outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
+
+    Return:
+        model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
+    """
     model_params = dict(input_size=feature_counts["backblaze"], hidden_size=512, num_layers=2, dropout=0.5)
-    opt_params = dict(batch_size=8, num_epochs=100, learning_rate=1e-3, optim_type=pt.optim.Adam)
+    opt_params = dict(**optimizer_default_config(), batch_size=8, num_epochs=100, learning_rate=1e-3)
     seed = 5002337303666687583
 
     model, metrics = do_model_run(
@@ -459,8 +464,6 @@ def BackblazeExtended_VanillaRNN(data_root_path: str, outputs_root: str):
     )
 
     return model, metrics
-
-
 
 
 # paper: MSE 6316, MFE -73.33, SMAPE 0.58, 0.57
@@ -474,10 +477,21 @@ BackblazeExtended_VanillaRNN.best_metrics = pd.DataFrame(
 )
 
 
-
 def BackblazeExtended_GRU(data_root_path: str, outputs_root: str):
+    """Train a GRU RNN on the BackblazeExtended Dataset.
+
+    Arguments:
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
+        outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
+
+    Return:
+        model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
+    """
     model_params = dict(input_size=feature_counts["backblaze"], hidden_size=512, num_layers=2, dropout=0.5)
-    opt_params = dict(batch_size=32, num_epochs=700, learning_rate=3e-3, schedule_T_mult=2, schedule_min=1e-5, optim_type=pt.optim.Adam)
+    opt_params = dict(**optimizer_default_config(), batch_size=32, num_epochs=700, learning_rate=3e-3)
     seed = 11068621650300516211
 
     model, metrics = do_model_run(
@@ -486,8 +500,6 @@ def BackblazeExtended_GRU(data_root_path: str, outputs_root: str):
     )
 
     return model, metrics
-
-
 
 
 # paper: MSE 924.9, MFE 14.11, SMAPE 0.37, SMPE -0.15
@@ -500,9 +512,22 @@ BackblazeExtended_GRU.best_metrics = pd.DataFrame(
     }
 )
 
+
 def BackblazeExtended_LSTM(data_root_path: str, outputs_root: str):
+    """Train an LSTM RNN on the BackblazeExtended Aggregate Dataset.
+
+    Arguments:
+        data_root_path: the root for the download of the dataset. Accelerates data loading (i.e. caching) and debugging data integrity.
+            Needs read-write permissions.
+        outputs_root: the root for the outputs emitted by the callbacks, as well as the metrics CSV. Needs write permissions
+
+    Return:
+        model: the trained model. There may be a model checkpoint that was better than this one, so we encourage you to compare
+            with the file model checkpoint output by ``ModelCheckpointer``
+        metrics: output of ``GeneralMetrics``
+    """
     model_params = dict(input_size=feature_counts["backblaze"], hidden_size=512, num_layers=2, dropout=0.5)
-    opt_params = dict(batch_size=8, num_epochs=100, learning_rate=1e-3, schedule_T_mult=2, schedule_min=1e-5, optim_type=pt.optim.Adam)
+    opt_params = dict(**optimizer_default_config(), batch_size=8, num_epochs=100, learning_rate=1e-3)
     seed = 15620825294243023828
 
     model, metrics = do_model_run(
@@ -511,8 +536,6 @@ def BackblazeExtended_LSTM(data_root_path: str, outputs_root: str):
     )
 
     return model, metrics
-
-
 
 
 # paper: MSE 610.86, MFE 14.45, SMAPE 0.37, SMPE -0.16
@@ -524,4 +547,3 @@ BackblazeExtended_LSTM.best_metrics = pd.DataFrame(
         "SMPE": [0.094],
     }
 )
-
